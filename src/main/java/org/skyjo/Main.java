b4361b3b06a6cb06753c91e26ca5d392c079aaf5
Ettotsu <eliott.speyser@free.fr>
2023-05-09 19:41:45 +0200

    Remove useless 2

--- src/main/java/org/skyjo/Main.java
+++ src/main/java/org/skyjo/Main.java
@@ -3,10 +3,7 @@ package org.skyjo;
 import org.skyjo.game.*;
 import org.skyjo.ui.UI;
 
-import javax.swing.SwingUtilities;
-
 public class Main {
-
     public static void main(String[] args) {
             Game game = new Game();
             UI window = new UI(game);
